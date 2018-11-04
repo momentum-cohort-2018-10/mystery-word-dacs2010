b0aee576f1a5f12f0588cda134e2632f89dc57be
xXDuckXx <dacs2010@gmail.com>
2018-11-04 15:17:19 -0500

    second commit

--- mystery_word.py
+++ mystery_word.py
@@ -1,4 +1,3 @@
-#import random module
 import random
 
 
@@ -10,45 +9,64 @@ with open("words.txt") as word_file:
         # remove all the new lines, set to upper case, and build a list
         word_list = [extra.strip().upper() for extra in word_list]
     # print(word_list)
-
-
-# go through list and get a random word 
+ 
 def random_word(word_list):
     '''
     random word generator
     '''
+    # iterate through the word list...
     for _ in word_list:  
+        # and select a random word
         return random.choice(word_list)
-# print(random_word(word_list))
 
+def win_condition(letters, game_word, correct_answer):
+    '''
+    check the letter against the game word, if they match user wins
+    '''
+    while correct_answer:
+        if letters == game_word:
+            correct_answer = True
+            print(correct_answer)
+            print("YOU WIN!!!")
+            break
+        else:
+            return False
 
-#take in user input(make sure it is only one letter)
-user_input = input("type a letter ").upper()
-# print(user_input)
+# def change_letters_to_blanks():
+#     for char in word 
+        
 
-
-def play_game(user_input):
+def play_game(game_word):
     '''
     take user input and check it against a random word
     '''
     game_word = random_word(word_list)
-    print(game_word)
+    print(game_word) # testing purposes (replace with "_" for letters function)
     letters = ""
-    guesses = 8
-    word_guessed = False
+    guesses = 7
+    correct_answer = True
     
+    # (can't exit loop... HELP)
+    while correct_answer is True or guesses > 0:
+        guess = input("type a letter => ").upper()
+        print("You have",+ guesses, "tries left ")
 
-    while  not word_guessed  and guesses > 0:
-        guess = user_input
-        print(guesses)
-        print(letters)
-        if letters == game_word:
-            word_guessed = True
+        # correct guess
         if guess in game_word:
-            letters = letters + user_input
-            print("good guess, try again")
-        else:
-            letters = letters = user_input
+            letters = letters + guess
+            win_condition(letters, game_word, correct_answer)
+            print("Guessed letters => " + letters)
+            print("Good guess, try again \n")
+
+        # incorrect guess
+        elif guess not in game_word:
             guesses -= 1
-            print("no good, try again")
-print(play_game(user_input))
+            letters = letters + guess
+            win_condition(letters, game_word, correct_answer)
+            print("Guessed letters => " + letters)
+            print("No good, try again \n")
+        else:
+            return
+
+# call the game, the random word generator, and the word list            
+print(play_game(random_word(word_list)))
